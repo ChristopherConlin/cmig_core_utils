cfa933f3b4a5811b5dd8d7b485c7dd291ebde242
Christopher Conlin <cconlin@health.ucsd.edu>
2025-08-28 16:20:36 -0700

    Added support for Siemens Aera XJ

--- gradient_nonlinearity/mmil_get_gradwarpinfo.m
+++ gradient_nonlinearity/mmil_get_gradwarpinfo.m
@@ -148,6 +148,8 @@ if ~isempty(regexpi(Manufacturer,'siemens'))
       gradwarpinfo.gwtype = 17;
     case {'cimax'}
       gradwarpinfo.gwtype = 18;
+    case {'aeraxj'}
+      gradwarpinfo.gwtype = 19;
     otherwise
       errmsg=sprintf('%s: Unknown gradient model %s %s\n',mfilename,Manufacturer,ManufacturersModelName);
       return;
